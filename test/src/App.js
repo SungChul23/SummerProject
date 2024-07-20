d5626c14da2a60c84445b95a2cc83e54fa061b0d
geondd <dansi03lee@gmail.com>
2024-07-20 21:48:09 +0900

    로그인 오류 수정

--- test/src/App.js
+++ test/src/App.js
@@ -23,7 +23,7 @@ function App() {
           />
           <Route
             path="/login"
-            Component={
+            element={
               <Layout>
                 <Header />
                 <Login />
