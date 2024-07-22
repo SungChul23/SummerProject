18c7ca235c412bcfcf10f1d6ac4bc6a44a189df5
geondd <dansi03lee@gmail.com>
2024-07-22 23:58:52 +0900

    로그인 이메일 비번 유효성 추가

--- test/src/Login.js
+++ test/src/Login.js
@@ -6,6 +6,7 @@ function Login() {
     const [userIdInput, setUserIdInput] = useState(' ');
     const [userPwInput, setUserPwInput] = useState(' ');
     const [showPassword, setShowPassword] = useState(false); //비번 숨기기/보이기 상태 변수
+    const [emailValid, setEmailValid] = useState(true);
 
     const handleChangeInput = (e) => {
         setUserIdInput(e.target.value);
@@ -13,6 +14,27 @@ function Login() {
 
     const handleShowPassword = () => {
         setShowPassword(!showPassword); // 보이기/숨기기 토글
+    };
+
+    const validateEmail = (email) => {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    };
+
+    const validatePassword = (password) => {
+        //비밀번호 유효성 검사 로직 추가(영문, 숫자, 특수문자 포함 8자 이상)
+        const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+        return re.test(password);
+    };
+
+    const handleBlur = (e) => {
+        const { name, value } = e.target;
+        if (name === 'userIdInput') {
+            setEmailValid(validateEmail(value)); // Check email validity
+        }
+        if (name === 'userPwInput'){
+            setUserPwInput(value);
+        }
     };
 
   return (
@@ -29,20 +51,21 @@ function Login() {
             </div>
             <div className="inputWrap">
             <input
-                name="userId"
+                name="userIdInput"
                 id="Id"
                 placeholder="이메일"
                 value={userIdInput}
                 onChange={handleChangeInput}
+                onBlur={handleBlur} //메일 유효성 검사
                 />
             </div>
         </div>
 
         {/* errror 메세지 띄우기  */}
-        <div className = "errorMessageWrap">
-            <div>올바른 이메일 주소를 입력해주세요</div>
-
-        </div>
+        {!emailValid && 
+            <div className="errorText">
+                올바른 이메일 주소를 입력해주세요
+            </div>}
 
         <div className = "contentWrap">
             <div className="inputTitle">
@@ -57,9 +80,10 @@ function Login() {
                     maxLength={16}
                     value={userPwInput}
                     onChange={(e) => setUserPwInput(e.target.value)} //비밀번호 입력 처리
+                    onBlur={handleBlur}
                 />
                 <div className="showHidePasswordButton">
-                    <button onClick={handleShowPassword}>
+                    <button onClick={handleShowPassword} className='shpb'>
                         {showPassword ? 'Hide' : 'Show'} 
                     </button>
                 </div>
@@ -67,10 +91,11 @@ function Login() {
         </div>
 
         {/* errror 메세지 띄우기  */}
-        <div className = "errorMessageWrap">
+        {!validatePassword(userPwInput) && (
+        <div className = "errorText">
             <div> 영문, 숫자, 특수문자 포함 8자 이상 입력해주세요</div>
-
         </div>
+        )}
 
         <div className='checkbutton'>
             <button type='button' className='btnLogin'>
